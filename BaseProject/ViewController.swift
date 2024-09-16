2e52e0bbfb0d7cbb09de8cb941b05248435c64a2
Hanan Ahmed <ahanan@eand.com>
2024-09-16 17:37:12 +0400

    Initial commit

---
+++ BaseProject/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  BaseProject
+//
+//  Created by Hanan Ahmed on 16/09/2024.
+//
+
+import UIKit
+import FeatureModule // Here to import and use the feature module
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    // MARK: - Actions
+    @IBAction func openCountryModule(_ sender: Any) {
+        let countryListViewController = CountryListConfigurator().setUpCountryListModule()
+        
+        self.navigationController?.pushViewController(countryListViewController, animated: true)
+        
+        // You can use coordinatore pattern to coordinate between modules
+    }
+    
+}
+
